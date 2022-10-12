cb83ed755164eb44e8ace13f296d827b3018f545
Khang Ng <nguyenhoaikhang37@gmail.com>
2022-10-12 16:48:25 +0700

    Done evolution section

---
+++ src/components/sections/Evolution/components/CardListDesktop/index.tsx
@@ -0,0 +1,46 @@
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import styles from "./CardListDesktop.scss?inline";
+import classNames from "classnames";
+
+import IMG_ROCKET from "~/assets/images/evolution/rocket.svg";
+
+import { cards } from "~/components/sections/Evolution";
+
+
+export default component$(() => {
+  useStylesScoped$(styles);
+
+  return (
+    <div class="hidden md:flex md:flex-row">
+      {cards.map((card, index) => (
+        <div className="card" key={index}>
+          <div class="card-group">
+            <p className="title">{card.title}</p>
+            <div class="image">
+              <img
+                src={card.image}
+                alt={card.title}
+                className="w-full object-contain"
+              />
+            </div>
+          </div>
+          <span
+            className={classNames(`label label-${index}`, {
+              ["label--soon"]: card.year === "Soon"
+            })}
+          >
+            {card.year}
+          </span>
+          {index === 2 && (
+            <img
+              src={IMG_ROCKET}
+              alt="rocket"
+              className="rocket-img w-full object-contain"
+            />
+          )}
+          <p className="content">{card.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+});
--- src/components/sections/Evolution/index.tsx
+++ src/components/sections/Evolution/index.tsx
@@ -1,7 +1,41 @@
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import styles from "./Evolution.scss?inline";
 
+import IMG_2013 from "~/assets/images/evolution/2013.svg";
+import IMG_2017 from "~/assets/images/evolution/2017.svg";
+import IMG_2021 from "~/assets/images/evolution/2021.svg";
+import IMG_NOW from "~/assets/images/evolution/now.svg";
+
 import Title from "~/components/Title";
+import CardListDesktop from "~/components/sections/Evolution/components/CardListDesktop";
+import CardListMobile from "~/components/sections/Evolution/components/CardListMobile";
+
+export const cards = [
+  {
+    title: "What is  blockchain?",
+    image: IMG_2013,
+    year: 2013,
+    content: "Bitcoin start viral",
+  },
+  {
+    title: "This is the future!",
+    image: IMG_2017,
+    year: 2017,
+    content: "ICO, blockchain become a new funding method",
+  },
+  {
+    title: "Web3 utilities",
+    image: IMG_2021,
+    year: 2021,
+    content: "Defi, GameFi, more scalable platform",
+  },
+  {
+    title: "Mass adoption",
+    image: IMG_NOW,
+    year: "Soon",
+    content: "A friendly way to use Web3",
+  },
+];
 
 export default component$(() => {
   useStylesScoped$(styles);
@@ -9,11 +43,12 @@ export default component$(() => {
   return (
     <div class="evolution_wrapper">
       <div class="evolution_wrapper_container">
-        <div class="flex flex-col gap-8">
-          <div class="my-0 mx-auto">
-            <Title title="Web3 evolution" />
-          </div>
-          <div class="">hello</div>
+        <div class="text-center">
+          <Title title="Web3 evolution" />
+        </div>
+        <div class="mt-[78px]">
+          <CardListDesktop />
+          <CardListMobile />
         </div>
       </div>
     </div>
