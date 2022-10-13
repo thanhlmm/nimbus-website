e49bf75c069d37e953a73534f5cf302248e111ac
Thanh Le <mihthanh27@gmail.com>
2022-10-14 06:45:53 +0700

    Add wow animation

--- src/root.tsx
+++ src/root.tsx
@@ -15,11 +15,16 @@ export default component$(() => {
     <QwikCity>
       <head>
         <meta charSet="utf-8" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g==" crossOrigin="anonymous" referrerPolicy="no-referrer"></script>
+        <link rel="stylesheet" href="/animate.css" />
         <RouterHead />
       </head>
       <body lang="en">
         <RouterOutlet />
         <ServiceWorkerRegister />
+        <script>
+          new WOW().init();
+        </script>
       </body>
     </QwikCity>
   );
