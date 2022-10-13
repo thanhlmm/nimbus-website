99fef1931c4886332a671792ef49f8a6299b0da9
Thanh Le <mihthanh27@gmail.com>
2022-10-13 15:46:42 +0700

    Fix css import

--- src/components/sections/Evolution/components/CardListDesktop/index.tsx
+++ src/components/sections/Evolution/components/CardListDesktop/index.tsx
@@ -1,5 +1,5 @@
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
-import styles from "./CardListDesktop.scss";
+import styles from "./CardListDesktop.scss?inline";
 import classNames from "classnames";
 
 import IMG_ROCKET from "~/assets/images/evolution/rocket.svg";
