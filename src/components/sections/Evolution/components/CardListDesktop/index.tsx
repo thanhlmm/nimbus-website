a65050080bb1fa87aeca477f6901be10b507da19
Khang Ng <nguyenhoaikhang37@gmail.com>
2022-10-17 21:16:59 +0700

    Change rocket animation

--- src/components/sections/Evolution/components/CardListDesktop/index.tsx
+++ src/components/sections/Evolution/components/CardListDesktop/index.tsx
@@ -1,4 +1,8 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import {
+  component$,
+  useStylesScoped$,
+  useClientEffect$,
+} from "@builder.io/qwik";
 import styles from "./CardListDesktop.scss?inline";
 import classNames from "classnames";
 
@@ -6,9 +10,55 @@ import IMG_ROCKET from "~/assets/images/evolution/rocket.svg";
 
 import { cards } from "~/components/sections/Evolution";
 
-
 export default component$(() => {
   useStylesScoped$(styles);
+
+  // useClientEffect$(() => {
+  //   // Get a reference to the <path>
+  //   var path = document.querySelector(".line");
+  //   console.log("✅", {path});
+  //
+  //   if (!path) return;
+  //
+  //   // Get length of path... ~577px in this case
+  //   var pathLength = path.clientWidth;
+  //   console.log({ pathLength });
+  //
+  //   // Make very long dashes (the length of the path itself)
+  //   path.style.width = pathLength + " " + pathLength;
+  //
+  //   // Offset the dashes so the it appears hidden entirely
+  //   path.style.width = pathLength;
+  //
+  //   // Jake Archibald says so
+  //   // https://jakearchibald.com/2013/animated-line-drawing-svg/
+  //   path.getBoundingClientRect();
+  //
+  //   // When the page scrolls...
+  //   window.addEventListener("scroll", function (e) {
+  //     // What % down is it?
+  //     // https://stackoverflow.com/questions/2387136/cross-browser-method-to-determine-vertical-scroll-percentage-in-javascript/2387222#2387222
+  //     // Had to try three or four differnet methods here. Kind of a cross-browser nightmare.
+  //     var scrollPercentage =
+  //       (document.documentElement.scrollTop + document.body.scrollTop) /
+  //       (document.documentElement.scrollHeight -
+  //         document.documentElement.clientHeight);
+  //
+  //     // Length to offset the dashes
+  //     var drawLength = pathLength * scrollPercentage;
+  //
+  //     // Draw in reverse
+  //     path.style.width = pathLength - drawLength;
+  //
+  //     // When complete, remove the dash array, otherwise shape isn't quite sharp
+  //     // Accounts for fuzzy math
+  //     if (scrollPercentage >= 0.99) {
+  //       path.style.width = "none";
+  //     } else {
+  //       path.style.width = pathLength + " " + pathLength;
+  //     }
+  //   });
+  // });
 
   return (
     <div class="hidden md:flex md:flex-row">
@@ -26,20 +76,25 @@ export default component$(() => {
           </div>
           <span
             className={classNames(`label`, {
-              ["label--soon"]: card.year === "Soon"
+              ["label--soon"]: card.year === "Soon",
             })}
           >
             {card.year}
-            <div className="wow bounceInLeft line" data-wow-duration="2s"
-                 data-wow-delay={index === 0 ? "0s" : index === 1 ? "1s" : "2s"} />
+            <div
+              className="line"
+              data-aos="fade-right"
+              data-aos-duration="500"
+              data-aos-delay={index === 0 ? "0" : index === 1 ? "500" : "1000"}
+            />
           </span>
           {index === 2 && (
             <img
               src={IMG_ROCKET}
               alt="rocket"
-              className="wow bounceInLeft rocket-img w-full object-contain"
-              data-wow-duration="3s"
-              data-wow-delay="3s"
+              className="rocket-img w-full object-contain"
+              data-aos="fade-right"
+              data-aos-delay="1800"
+              data-aos-duration="1500"
             />
           )}
           <p className="content">{card.content}</p>
