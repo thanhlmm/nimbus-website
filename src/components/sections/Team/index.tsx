3c3d9420783de29c617e96cfe0006b98cb13e1b3
binhnguyen2501 <binhq6@gmail.com>
2023-03-10 10:47:55 +0700

    feat: update quote

--- src/components/sections/Team/index.tsx
+++ src/components/sections/Team/index.tsx
@@ -16,7 +16,7 @@ export const teams = [
   {
     src: ToanNhu,
     quote:
-      "\"Drop down all those imaginations and feelings that 'Success is built on Success'. A HERO can be built from ZERO. Begin from where you stand with what you have! Nimbus is the best sidekick for your HERO LEGEND.\"",
+      '"A HERO can be built from ZERO. Begin from where you stand with what you have. Nimbus is best sidekick for your HERO LEGEND."',
     name: "Toan Nhu, Co - Founder",
   },
   {
@@ -41,8 +41,8 @@ export default component$(() => {
           {teams.slice(0, 3).map((item, index) => {
             return (
               <div class="card" key={index}>
-                <div class="w-[184px] h-[184px] overflow-hidden rounded-full p-2 hover:p-0 transition-all border-4 border-[#90e6e6] hover:border-[#41d0d0]">
-                  <div class="w-full h-full bg-[#90e6e6] relative overflow-hidden rounded-full scale-100 hover:scale-110 transition-all">
+                <div class="avatar">
+                  <div class="image_container">
                     <img
                       src={item.src}
                       class="w-full h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover"
@@ -50,7 +50,7 @@ export default component$(() => {
                   </div>
                 </div>
                 <div class="about_container">
-                  <div class="quote font-[400]">{item.quote}</div>
+                  <div class="quote">{item.quote}</div>
                   <div class="name">{item.name}</div>
                 </div>
               </div>
@@ -61,16 +61,17 @@ export default component$(() => {
           {teams.slice(0, 2).map((item, index) => {
             return (
               <div class="card" key={index}>
-                <div class="w-[184px] h-[184px] overflow-hidden rounded-full p-2 hover:p-0 transition-all border-4 border-[#5DC5C5] hover:border-[#41d0d0]">
-                  <img
-                    src={item.src}
-                    class="w-full h-full object-contain scale-100 hover:scale-110 transition-all"
-                  />
+                <div class="avatar">
+                  <div class="image_container">
+                    <img
+                      src={item.src}
+                      class="w-full h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover"
+                    />
+                  </div>
                 </div>
                 <div class="about_container">
                   <div class="quote">{item.quote}</div>
                   <div class="name">{item.name}</div>
-               
                 </div>
               </div>
             );
