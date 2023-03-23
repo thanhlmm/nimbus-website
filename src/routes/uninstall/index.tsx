3705abecb6be6d9ef1e557ca6bc0ce9ee5d6fb05
binhnguyen2501 <binhq6@gmail.com>
2023-03-24 00:09:35 +0700

    feat: intergrate feedbacks api

--- src/routes/uninstall/index.tsx
+++ src/routes/uninstall/index.tsx
@@ -1,5 +1,7 @@
-import { component$, useStylesScoped$, useStore } from "@builder.io/qwik";
+import { component$, useStylesScoped$, useStore, $ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import classNames from "classnames";
+import dayjs from "dayjs";
 
 import Title from "~/components/Title";
 import Button from "~/components/Button";
@@ -9,15 +11,89 @@ import styles from "./Uninstall.scss?inline";
 import Meme from "../../assets/images/delete.gif";
 
 interface HeaderStore {
-  text: string;
+  feedback: string;
+  email: string;
   isLoading: boolean;
+  errors: {
+    email: {
+      required: boolean;
+      msg: string;
+    };
+    feedback: {
+      required: boolean;
+      msg: string;
+    };
+  };
 }
 
 export default component$(() => {
   useStylesScoped$(styles);
   const state = useStore<HeaderStore>({
-    text: "",
+    feedback: "",
+    email: "",
     isLoading: false,
+    errors: {
+      email: {
+        required: false,
+        msg: "",
+      },
+      feedback: {
+        required: false,
+        msg: "",
+      },
+    },
+  });
+
+  const Submit = $(async () => {
+    const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    const form = {
+      date: dayjs(new Date()).format("DD/MM/YYYY - HH:mm"),
+      email: state.email,
+      feedback: state.feedback,
+    };
+
+    console.log("form: ", form);
+
+    if (form.feedback === "") {
+      state.errors.feedback = {
+        required: true,
+        msg: "Feedback is required",
+      };
+      return;
+    }
+
+    if (form.email && !regexEmail.test(form.email)) {
+      state.errors.email = {
+        required: true,
+        msg: "Email is not valid",
+      };
+      return;
+    }
+
+    state.isLoading = true;
+    try {
+      const res = fetch(
+        "https://aej6ifjhkj.execute-api.us-east-1.amazonaws.com/feedbacks",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(form),
+        }
+      );
+
+      const dataRes = await res;
+      console.log("dataRes: ", dataRes);
+
+      state.email = "";
+      state.feedback = "";
+    } catch (e) {
+      console.log("e: ", e);
+    } finally {
+      state.isLoading = false;
+    }
   });
 
   return (
@@ -36,21 +112,70 @@ export default component$(() => {
           </div>
         </div>
         <form
-          onSubmit$={async (ev) => {
-            (ev as FormDataEvent).preventDefault();
-            console.log("text: ", state.text);
-          }}
+          preventdefault:submit
+          onSubmit$={Submit}
           class="flex flex-col items-end gap-5"
         >
-          <textarea
-            rows={10}
-            cols={80}
-            placeholder="Leave some feedback here"
-            onInput$={(ev) =>
-              (state.text = (ev.target as HTMLTextAreaElement).value)
-            }
-            class="border border-[#27326F] placeholder-[#676e81] text-[#676e81] rounded-[4px] focus:outline-none p-4"
-          />
+          <div class="flex flex-col gap-4">
+            <div class="flex flex-col gap-1">
+              <input
+                placeholder="Let us know your email (optional)"
+                value={state.email}
+                onInput$={(ev) =>
+                  (state.email = (ev.target as HTMLTextAreaElement).value)
+                }
+                onBlur$={(ev) => {
+                  if ((ev.target as HTMLTextAreaElement).value) {
+                    state.errors.email = {
+                      required: false,
+                      msg: "",
+                    };
+                  }
+                }}
+                class={classNames(
+                  "border border-[#27326F] placeholder-[#676e81] text-[#676e81] rounded-[4px] focus:outline-none p-4 w-full",
+                  {
+                    ["border-red-500"]: state.errors.email.required,
+                  }
+                )}
+              />
+              {state.errors.email.required && (
+                <div class="text-red-500 font-medium">
+                  {state.errors.email.msg}
+                </div>
+              )}
+            </div>
+            <div class="flex flex-col gap-1">
+              <textarea
+                rows={10}
+                cols={80}
+                placeholder="Leave some feedback here"
+                value={state.feedback}
+                onInput$={(ev) =>
+                  (state.feedback = (ev.target as HTMLTextAreaElement).value)
+                }
+                onBlur$={(ev) => {
+                  if ((ev.target as HTMLTextAreaElement).value) {
+                    state.errors.feedback = {
+                      required: false,
+                      msg: "",
+                    };
+                  }
+                }}
+                class={classNames(
+                  "border border-[#27326F] placeholder-[#676e81] text-[#676e81] rounded-[4px] focus:outline-none p-4 w-full",
+                  {
+                    ["border-red-500"]: state.errors.feedback.required,
+                  }
+                )}
+              />
+              {state.errors.feedback.required && (
+                <div class="text-red-500 font-medium">
+                  {state.errors.feedback.msg}
+                </div>
+              )}
+            </div>
+          </div>
           <Button text="Send" type="submit" />
         </form>
       </div>
