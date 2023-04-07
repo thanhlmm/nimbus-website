03b0975b10d1de83cf006eddcbb2c163032c3f75
binhnguyen2501 <binhq6@gmail.com>
2023-04-07 22:07:57 +0700

    fix: update supported chain soon

--- src/components/sections/SupportedChain/index.tsx
+++ src/components/sections/SupportedChain/index.tsx
@@ -44,28 +44,52 @@ export default component$(() => {
       <div class="flex justify-center scale-100 hover:scale-110 transition-all mt-9">
         <img src={Ethereum} alt="Ethereum" loading="lazy" />
       </div>
+
       <div class="flex justify-center my-8">
         <div class="sub_title">And many more soon...</div>
       </div>
-      {/* <div class="grid md:grid-cols-4 grid-cols-1 md:gap-4 gap-6 items-center">
-        <div class="flex justify-center scale-100 hover:scale-110 transition-all">
-          <img src={Solona} alt="Solona" loading="lazy" />
+
+      <div class="flex flex-col gap-8">
+        <div class="grid md:grid-cols-3 grid-cols-1 md:gap-4 gap-6 items-center">
+          <div class="flex md:justify-end justify-center scale-100 hover:scale-110 transition-all">
+            <img src={Binance} alt="Binance" loading="lazy" />
+          </div>
+          <div class="flex justify-center scale-100 hover:scale-110 transition-all">
+            <img
+              src={Polygon}
+              alt="Polygon"
+              class="w-[163px] h-[34px] md:my-0 my-4"
+            />
+          </div>
+          <div class="flex md:justify-start justify-center scale-100 hover:scale-110 transition-all">
+            <img
+              src={Avalanche}
+              alt="Avalanche"
+              class="w-[242px] h-[46px]"
+              loading="lazy"
+            />
+          </div>
         </div>
-        <div class="flex justify-center scale-100 hover:scale-110 transition-all">
-          <img
-            src={Optimism}
-            alt="Optimism"
-            class="w-[177px] h-[46px] md:mt-0 mt-4"
-            loading="lazy"
-          />
+        <div class="grid md:grid-cols-4 grid-cols-1 md:gap-4 gap-6 items-center">
+          <div class="flex justify-center scale-100 hover:scale-110 transition-all">
+            <img src={Solona} alt="Solona" loading="lazy" />
+          </div>
+          <div class="flex justify-center scale-100 hover:scale-110 transition-all">
+            <img
+              src={Optimism}
+              alt="Optimism"
+              class="w-[177px] h-[46px] md:mt-0 mt-4"
+              loading="lazy"
+            />
+          </div>
+          <div class="flex justify-center scale-100 hover:scale-110 transition-all">
+            <img src={Arbitrum} alt="Arbitrum" loading="lazy" />
+          </div>
+          <div class="flex justify-center scale-100 hover:scale-110 transition-all">
+            <img src={Near} alt="Near" loading="lazy" />
+          </div>
         </div>
-        <div class="flex justify-center scale-100 hover:scale-110 transition-all">
-          <img src={Arbitrum} alt="Arbitrum" loading="lazy" />
-        </div>
-        <div class="flex justify-center scale-100 hover:scale-110 transition-all">
-          <img src={Near} alt="Near" loading="lazy" />
-        </div>
-      </div> */}
+      </div>
     </div>
   );
 });
