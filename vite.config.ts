c20ed01749885d62134b99ee960e476244308638
Thanh Le <mihthanh27@gmail.com>
2023-04-25 11:49:42 +0700

    Revert "Add cloudlfare"
    This reverts commit 13f3b8e9a7f2c9d697bdd69077cf6507ff408f42.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
-    ssr: { target: "webworker", noExternal: true },
+    ssr: { target: "node", format: "cjs" },
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
   };
 });
