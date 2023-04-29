d4bb07f1871c79d75e1d623b027428542aa554a1
Thanh Le <mihthanh27@gmail.com>
2023-04-29 14:48:42 +0700

    Add vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { qwikReact } from "@builder.io/qwik-react/vite";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig(() => {
   return {
     ssr: { target: "node", format: "cjs" },
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), qwikReact()],
+    plugins: [nodePolyfills({
+      // To exclude specific polyfills, add them to this list.
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }), qwikCity(), qwikVite(), tsconfigPaths(), qwikReact()],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
