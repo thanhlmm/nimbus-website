ddabc50244f97b362c7375b1c41a7bdc9528cfde
binhnguyen2501 <binhq6@gmail.com>
2023-06-02 13:55:52 +0700

    update style form

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -25,18 +25,23 @@ export default component$(() => {
       <Connect />
       {/* <Testimonials /> */}
       <Team />
-      <iframe
-        data-w-type="embedded"
-        src="https://08m2q.mjt.lu/wgt/08m2q/g7z/form?c=13dad1e6"
-        frameBorder="0"
-        scrolling="no"
-        marginHeight={0}
-        marginWidth={0}
+      <div
+        class="border border-[#0000001a] rounded-[20px] md:p-6 overflow-hidden"
         style="
           max-width: 1200px;
-          margin: -20px auto 100px auto;
-          width: 100%;"
-      ></iframe>
+          margin: 0 auto 120px auto;
+          width: 90%;"
+      >
+        <iframe
+          data-w-type="embedded"
+          src="https://08m2q.mjt.lu/wgt/08m2q/g7z/form?c=13dad1e6"
+          frameBorder="0"
+          scrolling="no"
+          marginHeight={0}
+          marginWidth={0}
+          style="width: 100%; margin-bottom: 50px;"
+        ></iframe>
+      </div>
     </>
   );
 });
