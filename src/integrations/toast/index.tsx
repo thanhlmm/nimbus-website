70eb9f55dbefcb55d118995105ac02933fb81bf1
binhnguyen2501 <binhq6@gmail.com>
2023-06-05 18:22:10 +0700

    add toast

---
+++ src/integrations/toast/index.tsx
@@ -0,0 +1,51 @@
+/** @jsxImportSource react */
+import { qwikify$ } from "@builder.io/qwik-react";
+import { Suspense } from "react";
+import toast, { Toaster } from "react-hot-toast";
+
+import "react-notion-x/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
+
+function ReactToast({ data }: { data: any }) {
+  const notify = () => {
+    toast.custom(
+      <Toast address="0x8980dbbe60d92b53b08ff95ea1aaaabb7f665bcb" />,
+      {
+        duration: 4000,
+        position: "bottom-left",
+        ariaProps: {
+          role: "status",
+          "aria-live": "polite",
+        },
+      }
+    );
+  };
+
+  return (
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Toaster />
+    </Suspense>
+  );
+}
+
+function Toast({ address }: any) {
+  const shorterAddress = (string: string) => {
+    return string ? string.slice(0, 6) + "..." + string.substr(-4) : string;
+  };
+
+  return (
+    <div className="flex items-center gap-2 bg-white py-[10px] px-[12px] rounded-lg">
+      <div className="text-lg text-white rounded-full flex justify-center items-center w-[22px] h-[22px] bg-[#b9bcc5]">
+        !
+      </div>
+      <div>
+        <b>{shorterAddress(address)}</b> has used Nimbus portfolio
+      </div>
+    </div>
+  );
+}
+
+// Specify eagerness to hydrate component on hover event.
+export const QReactToast = qwikify$(ReactToast, {
+  eagerness: "visible",
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
-import { component$, useStore } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+import { QReactToast } from "~/integrations/toast";
 import Hero from "~/components/sections/Hero";
 import Problem from "~/components/sections/Problem";
 import Evolution from "~/components/sections/Evolution";
@@ -12,33 +13,11 @@ import Connect from "~/components/sections/Connect";
 // import Testimonials from "~/components/sections/Testimonials";
 import Team from "~/components/sections/Team";
 import SignUpForm from "~/components/SignUpForm";
-import Toast from "~/components/Toast";
-
-interface StoreType {
-  openToast: boolean;
-}
 
 export default component$(() => {
-  const state = useStore<StoreType>({
-    openToast: false,
-  });
-
-  const shorterAddress = (string: string) => {
-    return string ? string.slice(0, 6) + "..." + string.substr(-4) : string;
-  };
-
   return (
     <>
-      {/* <div
-        onClick$={() => {
-          state.openToast = true;
-          setTimeout(() => {
-            state.openToast = false;
-          }, 2000);
-        }}
-      >
-        hello
-      </div> */}
+      <QReactToast data="hello" />
       <Hero />
       <Problem />
       <Evolution />
@@ -52,17 +31,6 @@ export default component$(() => {
       <div class="form_wrapper">
         <SignUpForm />
       </div>
-
-      {state.openToast && (
-        <Toast
-          message={`User Thanh Le with wallet address ${shorterAddress(
-            "0x8980dbbe60d92b53b08ff95ea1aaaabb7f665bcb"
-          )} has been added to Nimbus`}
-          type="info"
-          isShow={state.openToast}
-          position="bottom-right"
-        />
-      )}
     </>
   );
 });
