a1c812af725297bf57efd047027537fa46ac3187
huong666 <55019690+huong666@users.noreply.github.com>
2023-06-08 14:56:32 +0700

    feat: new toast

--- src/integrations/toast/index.tsx
+++ src/integrations/toast/index.tsx
@@ -1,51 +1,77 @@
 /** @jsxImportSource react */
 import { qwikify$ } from "@builder.io/qwik-react";
-import { Suspense } from "react";
-import toast, { Toaster } from "react-hot-toast";
+import toast, { Toaster, ToastBar } from "react-hot-toast";
+import { AiOutlineExclamationCircle } from "react-icons/ai";
 
-import "react-notion-x/src/styles.css";
-import "prismjs/themes/prism-tomorrow.css";
-
-function ReactToast() {
+const ReactToast = () => {
   const notify = () => {
-    toast.custom(
-      <Toast address="0x8980dbbe60d92b53b08ff95ea1aaaabb7f665bcb" />,
-      {
-        duration: 4000,
-        position: "bottom-left",
-        ariaProps: {
-          role: "status",
-          "aria-live": "polite",
-        },
-      }
-    );
+    let address = "0x8980dbbe60d92b53b08ff95ea1aaaabb7f665bcb";
+    toast(<Toast address={address} />, {
+      duration: 3000,
+      position: "bottom-left",
+      style: {
+        borderRadius: "5px",
+        minWidth: "300px",
+        width: "25%",
+        visibility: "visible",
+        verticalAlign: "middle",
+      },
+      ariaProps: {
+        role: "status",
+        "aria-live": "polite",
+      },
+    });
   };
-
   return (
-    <Suspense fallback={<h1>Loading...</h1>}>
-      <Toaster />
-    </Suspense>
+    <div>
+      <Toaster>
+        {(t) => (
+          <ToastBar toast={t}>
+            {({ icon, message }) => (
+              <>
+                {icon}
+                {message}
+                {t.type !== "loading" && (
+                  <div>
+                    <button
+                      style={{
+                        border: "0px",
+                        fontSize: "12px",
+                        backgroundColor: "white",
+                      }}
+                      onClick={() => toast.dismiss(t.id)}
+                    >
+                      X
+                    </button>
+                  </div>
+                )}
+              </>
+            )}
+          </ToastBar>
+        )}
+      </Toaster>
+    </div>
   );
-}
+};
 
-function Toast({ address }: any) {
+const Toast = ({ address }: any) => {
   const shorterAddress = (string: string) => {
     return string ? string.slice(0, 6) + "..." + string.substr(-4) : string;
   };
 
   return (
-    <div className="flex items-center gap-2 bg-white py-[10px] px-[12px] rounded-lg">
-      <div className="text-lg text-white rounded-full flex justify-center items-center w-[22px] h-[22px] bg-[#b9bcc5]">
-        !
-      </div>
+    <div>
       <div>
-        <b>{shorterAddress(address)}</b> has used Nimbus portfolio
+        <b>
+          <AiOutlineExclamationCircle
+            style={{ color: "rgb(159, 159, 159)", width: "20px", margin: 2, position: "relative", top: "2.5px"}}
+          />{" "}
+          {shorterAddress(address)}
+        </b>{" "}
+        has used Nimbus portfolio
       </div>
     </div>
   );
-}
+};
 
-// Specify eagerness to hydrate component on hover event.
-export const QReactToast = qwikify$(ReactToast, {
-  eagerness: "visible",
-});
+export const QReactToast = qwikify$(ReactToast, { eagerness: "hover" });
